0353fa0626e4e840c1cc09b710ff50ff3a18c11f
Brendan Cheng <bchen022@hammer.cs.ucr.edu>
2014-10-21 13:17:22 -0700

    got && to work but exit now malfunctioning

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <unistd.h>			//fork, execvp
 #include <sys/wait.h>		//waitpid
 #include <errno.h>			//perror
-#include <stdio.h>			//perror, exit (same as iostream?)
+#include <stdio.h>			//perror, exit 
 #include <boost/tokenizer.hpp>	//for parsing
 #include <string>
 #include <string.h>
@@ -37,27 +37,49 @@ int main() {
 			
 			cout << userid << "@" << hostname << "$ ";
 
-			string commands;	
+			string commands;
+			int and_flag = 0;
+			int or_flag = 0;
 			getline(cin, commands);														//get user input
-
 			char* saveptr1;
 			char* saveptr2;
-			char* c_commands = new char [commands.length() + 1];					//create a c-string for strtok's argument
-			strcpy(c_commands, commands.c_str());									//fill in the c-string with user input
-			for (int cmtfind = 0; cmtfind < strlen(c_commands); ++cmtfind) {
+			char* c_commands = new char [commands.length() + 1];						//create a c-string for strtok's argument
+			strcpy(c_commands, commands.c_str());										//fill in the c-string with user input
+			for (unsigned cmtfind = 0; cmtfind < strlen(c_commands); ++cmtfind) {
 				if (c_commands[cmtfind] == '#') {
 					c_commands[cmtfind] = '\0';
 				}
+				if (c_commands[cmtfind] == '&') {
+					if (c_commands[cmtfind + 1] == '&') {
+						cout << "and_flag was incremented. " << endl;
+						and_flag = 1;
+					}				
+				}
+				if (c_commands[cmtfind] == '|') {
+					if (c_commands[cmtfind + 1] == '|') {
+						cout << "or_flag was incremented. " << endl;
+						or_flag = 1;
+					}
+				}
+
 			}
-		
-			char* argv_tok = strtok_r(c_commands,";", &saveptr1);					//parse the c-string(user input)
-		
+	
+			char delim_list[] = ";||&&";
+			char* argv_tok = strtok_r(c_commands, delim_list, &saveptr1);					//parse the c-string(user input)
+
 			vector<char*> argv_vect;
 			vector<char*> argv_vect2;
 			int i = 0;
-			while (argv_tok != NULL) {												//fill vectors with parsed user input
+			while (argv_tok != NULL) {													//fill vectors with parsed user input
 				argv_vect.push_back(argv_tok);
-				char* argv_tok2 = strtok_r(argv_vect.at(i), " ", &saveptr2);
+				if (argv_vect.size() > 0) {
+					for (unsigned argv_ctr = 0; argv_ctr < argv_vect.size(); ++argv_ctr) {
+						cout << "contents of argv_vect1: " << argv_vect.at(argv_ctr) << endl;
+					}
+
+				}
+				
+				char* argv_tok2 = strtok_r(argv_vect.at(i), " " , &saveptr2);
 				while (argv_tok2 != NULL) {
 					argv_vect2.push_back(argv_tok2);
 					argv_tok2 = strtok_r(NULL, " ", &saveptr2);
@@ -65,52 +87,88 @@ int main() {
 
 				int command_sz = argv_vect2.size();
 				char** argv = new char*[command_sz + 1]; 								//create argv for execvp
-				for (int j = 0; j < argv_vect2.size(); ++j) {
+				for (unsigned j = 0; j < argv_vect2.size(); ++j) {
 					argv[j] = new char[sizeof(argv_vect2.at(j)) + 1];					//filling in each index of argv with
 					strcpy(argv[j], argv_vect2.at(j));									//enough space for cstring commands
 				}
-				argv[argv_vect2.size()] = NULL;
+				for (unsigned argv2_ctr = 0; argv2_ctr < argv_vect2.size(); ++argv2_ctr) {
+					cout << "contents of argv_vect2: " << argv_vect2.at(argv2_ctr) << endl;
+				}
+				for (unsigned argv3_ctr = 0; argv3_ctr < argv_vect2.size(); ++argv3_ctr) {
+					cout << "contents of argv array: " << argv[argv3_ctr] << endl;
+				}
 			
+				argv[argv_vect2.size()] = NULL;
 
 				string exitor = "exit";
 				string exitor2 = " exit";
-				for (int m = 0; m < argv_vect.size(); ++m) {
+				for (unsigned m = 0; m < argv_vect.size(); ++m) {
+					cout << "entered loop. " << endl;
 					int exit_comp = strcmp(argv_vect.at(m), exitor.c_str());
 					int exit_comp2 = strcmp(argv_vect.at(m), exitor2.c_str());
 					if (exit_comp == 0 || exit_comp2 == 0) {
+						cout << "exited" << endl;
 						exit(1);
 					}
 				}
-			
+				cout << "finished loop. " << endl;	
 				int pid2 = fork();
+				cout << "pid 2: " << pid2 << endl;
+				int status = 0;
 				if (pid2 == -1) {
 					perror("Unable to fork for execvp.");
+					exit(1);
 				}
 				if (pid2 == 0) {
+					cout << "entered child branch." << endl;
+					cout << "OR_FLAG: " << or_flag << endl;
+					cout << "AND_FLAG: " << and_flag << endl;	
+					
+					/*if (or_flag > 0) {
+						
+
+
+					}*/
+				
 					if (-1 == execvp(argv[0], argv)) {
 						perror("There was an error in execvp. ");
+						if (and_flag > 0) {
+							status = 3;
+							cout << "should exit because AND was found. " << endl;
+						//	exit(1);
+							break;
+						}
+
+						if (or_flag > 0) {
+							status = 3;
+							cout << "should continue because OR was found2. " << endl;
+							exit(1);
+						//	shouldn't this mean the next command will work?
+						//	break;
+						}
 						exit(1);
+					}
+					else { 
+						if (or_flag > 0) {
+							status = 3;
+							cout << "should exit because OR was found. " << endl;
+							break;
+						}
 					}
 				}
 				if (pid2 > 0) {
-					if (-1 == wait(0)) {
+					cout << "entered parent branch. " << endl;
+					if (-1 == wait(&status)) {
 						perror("Still waiting for child execvp process to end.");
 					}
 				}
 				
-				argv_tok = strtok_r(NULL, ";", &saveptr1);
+				argv_tok = strtok_r(NULL, delim_list, &saveptr1);
 
 				++i;
 				argv_vect2.clear();
 			}
-
-			/*for (int z = 0; z < argv_vect.size(); ++ z) {
-				cout << "contents of argv_vect1: " << argv_vect.at(z) << endl;
-			}*/
-		
 		}
-		
-
 	}
 
 	else if (pid > 0) {
@@ -120,13 +178,4 @@ int main() {
 		}
 	}
 }
-
-
-
-
-
-
-
-
-
 
