6b4668883877627b4a26a74de9da077353a6563d
Brendan Cheng <bchen022@hammer.cs.ucr.edu>
2014-10-25 21:28:17 -0700

    distinguished between directories or flags

--- src/ls.cpp
+++ src/ls.cpp
@@ -1,7 +1,12 @@
-#include <sys/types.h>
-#include <dirent.h>
-#include <errno.h>
+#include <sys/types.h>		//stat, closedir, opendir
+#include <dirent.h>			//readdir, closedir, opendir
+#include <errno.h>			//perror
+#include <stdio.h>			//exit, perror
 #include <iostream>
+#include <vector>
+#include <string>
+#include <unistd.h>			// stat
+#include <sys/stat.h>		// stat
 using namespace std;
 
 /* This is a BARE BONES example of how to use opendir/readdir/closedir. Notice
@@ -9,12 +14,38 @@ that there is no error checking on these functions. You MUST add error
  checking yourself.
 */
 
-int main() {
+int main(int argc, char* argv[]) {
+
+	vector<string> arg_list;
+	vector<string> directories;
+	vector<string> user_arg;
+
+	for (unsigned i = 0; i < argc; ++i) {				//fill in a vector with the passed in arguments
+		arg_list.push_back(argv[i]);
+	}
+
+	for (unsigned i = 0; i < arg_list.size(); ++i) {			//distinguish between directories and arguments
+		if (arg_list.at(i).at(0) != '-') { 
+			string adirectory = arg_list.at(i);
+			directories.push_back(adirectory);
+		}
+		
+		else if (arg_list.at(i).at(0) == '-') {
+			string anargument = arg_list.at(i);
+			user_arg.push_back(anargument);
+		}
+
+	}
+
+
 	char *dirName = ".";
 	DIR *dirp = opendir(dirName)
-	dirent *direntp;
-	while ((direntp = readdir(dirp)))
-	cout << direntp->d_name << endl; // use stat here to find attributes of file
+	dirent *direntp;								
+	while ((direntp = readdir(dirp)))	{ //readdir returns a pointer to the next directory 
+										  //(which is a dirent struct that is pointed to by dirp)
+		
+		cout << direntp->d_name << endl; // use stat here to find attributes of file
+	}
 	closedir(dirp);
 }
 
