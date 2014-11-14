60acedcdc2317394a357191d41f2a273f83b826b
Brendan Cheng <bchen022@hammer.cs.ucr.edu>
2014-11-14 14:29:10 -0800

    still transfering

--- src/main.cpp
+++ src/main.cpp
@@ -48,7 +48,56 @@ int main() {
 		getline(cin, commands);														//get user input
 		char* saveptr1;
 		char* saveptr2;
+		char* c_commands = new char [commands.length() + 1];						//create a c-string for strtok's argument
+		strcpy(c_commands, commands.c_str());										//fill in the c-string with user input
+		for (unsigned cmtfind = 0; cmtfind < strlen(c_commands); ++cmtfind) {
+			if (c_commands[cmtfind] == '#') {
+				c_commands[cmtfind] = '\0';
+			}
+			if (c_commands[cmtfind] == '&') {
+				if (c_commands[cmtfind + 1] == '&') {
+					and_flag = 1;
+				}				
+			}
+			if (c_commands[cmtfind] == '|') {
+				if (c_commands[cmtfind + 1] == '|') {
+					or_flag = 1;
+				}
+				else {
+					//piping
+					pipe_flag = 1;
+				}
+			}
+			if (c_commands[cmtfind] == '<') {
+				//input redirection
+				i_flag = 1;
+			}
+			if (c_commands[cmtfind] == '>') {
+				if (c_commands[cmtfind + 1 ] == '>') {
+					//>> don't delete whats in the file (the file you are redirecting the output into)
+					oo_flag = 1;
+				}
+				else {
+					//> delete whats in the file you are redirecting into
+					o_flag = 1;
+				}
+			}
+		}
+	
+		char delim_list[] = ";|&";
+		char* argv_tok = strtok_r(c_commands, delim_list, &saveptr1);					//parse the c-string(user input)
 
+		vector<char*> argv_vect;
+		vector<char*> argv_vect2;
+		while (argv_tok != NULL) {													//fill vectors with parsed user input
+			argv_vect.clear();
+			argv_vect.push_back(argv_tok);
+			if (argv_vect.size() > 0) {
+				for (unsigned argv_ctr = 0; argv_ctr < argv_vect.size(); ++argv_ctr) {
+					cout << "contents of argv_vect1: " << argv_vect.at(argv_ctr) << endl;
+				}
+			}
+			
 
 
 
